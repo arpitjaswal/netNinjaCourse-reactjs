e696ea144e9d903f2414aa4064a540eeadfe9ce3
arpitjaswal <iamarpitjaswal@gmail.com>
2023-03-19 17:17:46 +0530

    #5 Multiple components: learnt how to create multiple components inside a reactjs webapp

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import './App.css';
-
+import HomePage from './components/HomePage';
+import NavBar from './components/NavBar';
 function App() {
-  const val = "Welcome to the blog";
-  const likes = 50;
-  const link = "https://www.linkedin.com/in/arpitjaswal/";
+  // const val = "Welcome to the blog";
+  // const likes = 50;
+  // const link = "https://www.linkedin.com/in/arpitjaswal/";
   // const objperson = {name: "Arpit", age: 23, email: "iamarpitjaswal@gmail.com"}
   return (
     <div className="App">
-      <div className='content'>
-        <h1>{val}</h1>
-        <p>Liked {likes} times!</p>
-        {/* <p>{objperson}</p> */}
-        <p>{10}</p>
-        <p>{"hello ninjas!"}</p>
-        <p>{[1,2,3,4,5]}</p>
-        <p>{Math.random()*10}</p>
-        <a href={link}>My Linkedin profile</a>
-      </div>
+      <NavBar/>
+      <h1>App component</h1>
+      <HomePage/>
     </div>
   );
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <nav className="navbar">
+        <h1>My arpitrary thoughts</h1>
+        <div className='links'>
+            <a href="/">Home</a>
+            <a href="/create">New Blog</a>
+
+        </div>
+    </nav>
+  )
+}
+
+export default NavBar;
