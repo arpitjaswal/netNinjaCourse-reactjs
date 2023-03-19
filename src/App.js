0b24c8cf867cdbbddae5896f47be283b6b055989
arpitjaswal <iamarpitjaswal@gmail.com>
2023-03-19 16:55:48 +0530

    #4 Dynamic values in templates: learnt how to add dynamic values inside the tags for data that changes on the go eg: blog titles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
 
 function App() {
+  const val = "Welcome to the blog";
+  const likes = 50;
+  const link = "https://www.linkedin.com/in/arpitjaswal/";
+  // const objperson = {name: "Arpit", age: 23, email: "iamarpitjaswal@gmail.com"}
   return (
     <div className="App">
       <div className='content'>
-        <h1>App component</h1>
+        <h1>{val}</h1>
+        <p>Liked {likes} times!</p>
+        {/* <p>{objperson}</p> */}
+        <p>{10}</p>
+        <p>{"hello ninjas!"}</p>
+        <p>{[1,2,3,4,5]}</p>
+        <p>{Math.random()*10}</p>
+        <a href={link}>My Linkedin profile</a>
       </div>
     </div>
   );
