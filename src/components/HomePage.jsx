1ee96a673af03128ba0fb009d2491d53f4dbb2ec
arpitjaswal <iamarpitjaswal@gmail.com>
2023-03-20 16:46:33 +0530

    #7  Click events: learnt how to handle clicks(events) in a react app and how to pass dynamic values using callback functions

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 
-const HomePage = () => {
+const HomePage = (e) => {
+    const handleClick = ()=>{
+        console.log("Clicked!",e);
+    }
+    const handleclickagain= (name,e)=>{
+        console.log("Clicked again, "+name+e);
+    }
   return (
-    <div className='content'>Home Page</div>
+    <>
+        <div className='content'>Home Page</div>
+        <button onClick={handleClick}>Click Me!</button>
+        <button onClick={(e)=>{
+            handleclickagain("Bro",e);
+        }}>Click me again!</button>
+    </>
   )
 }
 
