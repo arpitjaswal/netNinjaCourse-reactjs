efce1089fd893da914fa380cae8589e98591860d
arpitjaswal <iamarpitjaswal@gmail.com>
2023-04-12 03:51:08 +0530

    deleted node_modules folder

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import BlogList from "./BlogList"
 import { useState } from 'react';
 
@@ -14,6 +14,10 @@ const HomePage = () => {
       const newBlogs = blogs.filter((b)=>b.id!==id);
       setBlogs(newBlogs);
     }
+
+    useEffect(()=>{
+      console.log("useEffect fired!")
+    })
   return (
     <div className='home'>
         <BlogList blogs={blogs} title = "My blogs" handleDelete={handleDelete}/>
